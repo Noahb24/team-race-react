96d4e4fd81e32ad00026dff96fce133c9ad596fc
block <73147603+Noahb24@users.noreply.github.com>
2022-04-25 17:04:05 -0600

    add delete racer to team maker

--- src/components/teamGen/racer.js
+++ src/components/teamGen/racer.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { racerChange, reset, selectRacers } from './teamMakerSlice'
+import { racerChange, reset, selectRacers, deleteRacer } from './teamMakerSlice'
 
 function Racer() {
     const racers = useSelector(selectRacers)
     const dispatch = useDispatch()
 
     function handleRacerChange (num, type, value) {
-
         const payload = {
             type,
             num,
@@ -17,13 +16,17 @@ function Racer() {
         dispatch(reset('teams'))
     }
 
+    function handleDeleteRacer (num) {
+        dispatch(deleteRacer(num))
+    }
+
     return (
         <div className = "racers">
             {   
             racers.map( (element, i) => {
                 return(
                     <div key={i} className="racer">
-                        <p>Name: <input id="name" type="text" onChange ={e => handleRacerChange(i, 'name', e.target.value)} value={racers[i].name}></input> -- Minute: <input id="time" type="number" onChange ={e => handleRacerChange(i, 'minute', Number(e.target.value))} value={racers[i].minute}></input> - Second: <input id="time" type="number" onChange ={e => handleRacerChange(i, 'second', Number(e.target.value))} value={racers[i].second}></input> - Millisecond: <input id="time" type="number" onChange ={e => handleRacerChange(i, 'ms', Number(e.target.value))} value={racers[i].ms}></input></p>
+                        <p><button onClick={() => handleDeleteRacer(i)}>Delete</button>Name: <input id="name" type="text" onChange ={e => handleRacerChange(i, 'name', e.target.value)} value={racers[i].name}></input> -- Minute: <input id="time" type="number" onChange ={e => handleRacerChange(i, 'minute', Number(e.target.value))} value={racers[i].minute}></input> - Second: <input id="time" type="number" onChange ={e => handleRacerChange(i, 'second', Number(e.target.value))} value={racers[i].second}></input> - Millisecond: <input id="time" type="number" onChange ={e => handleRacerChange(i, 'ms', Number(e.target.value))} value={racers[i].ms}></input></p>
                     </div>
                     )
                 })
