5ef0f8fbd5c658214825552f01231d5e85db6a31
block <73147603+Noahb24@users.noreply.github.com>
2022-04-26 08:13:32 -0600

    add log rocket

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter} from "react-router-dom";
+import LogRocket from 'logrocket';
 require("dotenv").config({path: '../config.env'});
 
+
+if(process.env.NODE_ENV !== "development"){
+  LogRocket.init('2p8jrv/team-race');
+  console.log('Using Log Rocket')
+}
 
 ReactDOM.render(
   <React.StrictMode>
