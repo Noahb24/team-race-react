f44b045d110a83da43961ea088b563768849453e
block <73147603+Noahb24@users.noreply.github.com>
2022-08-09 16:53:26 -0600

    add podcast episode 3

--- src/components/home/Highlights.js
+++ src/components/home/Highlights.js
@@ -7,7 +7,7 @@ const Highlights = () => {
     return (
         <div className='homeComponent ms-auto' id='highlightSideBar'>
             <h1>Recent Updates</h1>
-            <iframe width="420" height="236" src="https://www.youtube.com/embed/7RP4vnPuJ1o" 
+            <iframe width="420" height="236" src="https://www.youtube.com/embed/cS8a_S6G7VM" 
                             title="YouTube video player" frameborder="0" 
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                             className='podcast'
@@ -15,10 +15,12 @@ const Highlights = () => {
             <h1 id='pointsHeader'>Current Points</h1>
             <div className='points' id='teamPoints'>
                 <h4>Team Race Points</h4>
-                <p>Alisha - 1 +</p>
-                <p>Noah - 1 -- Sean - 1 -- Chuck - 1</p>
-                <p>BJ - 2 -- John - 2 -- Kelly - 2</p>
-                <p>Kris - 3 -- Derek - 3 -- Ed - 3</p>
+                <p>John - 3</p>
+                <p>BJ - 4 -- Chuck - 4</p>
+                <p>AJ - 5 -- Kali - 5 -- Kelly - 5 -- Clayton - 5</p>
+                <p>Noah - 6 -- Mcnulty - 6 -- Dakota - 6 -- Ed - 6</p>
+                <p>Kris - 7</p>
+                <p>Shaun - 8 -- Derek - 8</p>
             </div>
             <div className='points' id='kidsPoints'>
                 <h4>Youth Race Points</h4>
--- src/components/podcasts/podcasts.js
+++ src/components/podcasts/podcasts.js
@@ -32,11 +32,15 @@ const Podcasts = () => {
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                             className='podcast'
                             allowfullscreen></iframe>
+                    <iframe width="420" height="236" src="https://www.youtube.com/embed/cS8a_S6G7VM" 
+                            title="YouTube video player" frameborder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                            className='podcast'
+                            allowfullscreen></iframe>
                 </div>
             </div>
         
         </div>
-       
 
     )
 }
